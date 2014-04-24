829c4eb68ed3aef424face4276ffa1a17a5da63c
Gagan.Mishra <gagan@AQRGGNF12W5.local>
2014-04-24 18:24:37 +0530

    Initial Commit

---
+++ BlockProgramming/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BlockProgramming
+//
+//  Created by Gagan on 24/04/14.
+//  Copyright (c) 2014 Gagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BlockProgramming/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlockProgramming
+//
+//  Created by Gagan on 24/04/14.
+//  Copyright (c) 2014 Gagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
